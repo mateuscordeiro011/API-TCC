f2fafb6ff0fc4f0ee68ca2ee4c2343f1c9fcc372
Mateus Cordeiro da Silva <rm91820@estudante.fieb.edu.br>
2025-08-08 17:50:35 -0300

    Refazendo Endereço, adicionando Adoção e configurando Domain

---
+++ src/main/java/INF2BN_2024_0_EQUIPE02/api/domain/Adocao.java
@@ -0,0 +1,92 @@
+package INF2BN_2024_0_EQUIPE02.api.domain;
+
+import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "Adocao", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "Id_animal")
+})
+public class Adocao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id_adocao")
+    private Long id_adocao;
+
+    @Column(name = "Data_adocao", nullable = true)
+    private LocalDate data_adocao;
+
+    @Column(name = "Status", nullable = true)
+    private boolean status;
+
+    @Column(name = "Observacoes", nullable = true, length = 500)
+    private String observacoes;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @JoinColumn(name = "Id_animal", nullable = true)
+    private Animal animal;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @JoinColumn(name = "Id_cliente", nullable = true)
+    private Cliente cliente;
+
+    public Long getId_adocao() {
+        return id_adocao;
+    }
+
+    public void setId_adocao(Long id_adocao) {
+        this.id_adocao = id_adocao;
+    }
+
+    public LocalDate getData_adocao(){
+        return data_adocao;
+    }
+
+    public void setData_adocao(LocalDate data_adocao){
+        this.data_adocao = data_adocao;
+    }
+
+    public boolean getStatus(){
+        return status;
+    }
+
+    public void setStatus(boolean status){
+        this.status = status;
+    }
+
+    public String getObservacoes(){
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes){
+        this.observacoes = observacoes;
+    }
+
+    
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    
+        public Animal getAnimal() {
+            return animal;
+        }
+
+        public void setAnimal(Animal animal) {
+            this.animal = animal;
+        }
+}
--- src/main/java/INF2BN_2024_0_EQUIPE02/api/domain/Endereco.java
+++ src/main/java/INF2BN_2024_0_EQUIPE02/api/domain/Endereco.java
@@ -15,8 +15,8 @@ public class Endereco {
     @Column(name = "Cep", nullable = true, length = 15)
     private String cep;
 
-    @Column(name = "Logradouro", nullable = true, length = 100)
-    private String logradouro;
+    @Column(name = "Rua", nullable = true, length = 100)
+    private String rua;
 
     @Column(name = "Bairro", nullable = true, length = 50)
     private String bairro;
@@ -26,4 +26,42 @@ public class Endereco {
 
     @Column(name = "Estado", nullable = true, length = 50)
     private String estado;
+
+    @Column(name = "Complemento", nullable = true, length = 20)
+    private String complemento;
+
+    @Column(name = "Numero", nullable = true, length = 10)
+    private int numero;
+
+    public Long getId_endereco() {
+        return id_endereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
 }
